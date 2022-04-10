edad525d08b6e585bcd60196b1752de57005211a
tanukamble <99479605+tanukamble@users.noreply.github.com>
2022-04-10 14:45:29 +0530

    Add files via upload

---
+++ Document_files/index.js
@@ -0,0 +1,63 @@
+
+//minumum of the three number
+const num = {
+    firstnum : 15,
+    secondnum : 30,
+    thirdnum : 10,
+
+};
+    console.log(num);
+    document.write('num');
+    let ans=isVowel('A');
+    console.log(ans);
+
+
+//given charecter is ovel or not
+function isVowel(x){
+
+    let result;
+
+        if(x == "A" || x == "E" || x == "I" || x == "O" || x == "U" ) {
+            result = true;
+        }
+        else{
+            result = false;
+        }
+    return result;
+     console.log('result');
+    };
+    
+    
+    //program to check if a number is prime or not
+
+    // take input from the user
+    const number = parseInt(prompt("Enter a positive number: "));
+    let isPrime = true;
+    
+    // check if number is equal to 1
+    if (number === 1) {
+        console.log("1 is neither prime nor composite number.");
+    }
+    
+    // check if number is greater than 1
+    else if (number > 1) {
+    
+        // looping through 2 to number-1
+        for (let i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    
+        if (isPrime) {
+            console.log(`${number} is a prime number`);
+        } else {
+            console.log(`${number} is a not prime number`);
+        }
+    }
+    
+    // check if number is less than 1
+    else {
+        console.log("The number is not a prime number.");
+    };
