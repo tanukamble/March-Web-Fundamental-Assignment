8cb315ded6f9f34c43106714bac97a65223959b9
tanukamble <99479605+tanukamble@users.noreply.github.com>
2022-05-01 12:54:12 +0530

    Add files via upload

---
+++ navbar2/index.js
@@ -0,0 +1,36 @@
+const homeBtn = document.querySelector("#home")
+const booksBtn = document.querySelector("#books")
+const kindleBtn = document.querySelector("#kindle")
+
+const homePara = document.querySelector("#home-p")
+const booksPara = document.querySelector("#books-p")
+const kindlePara = document.querySelector("#kindle-p")
+
+homeBtn.addEventListener('click', displayHome)
+
+function displayHome(){
+    homeBtn.classList.add('btn-active')
+    // homePara.classList.remove("inactive")
+    // homePara.classList.add('active')
+    homePara.classList.toggle('inactive')
+}
+
+booksBtn.addEventListener('click', displayBooks)
+
+function displayBooks(){
+    booksBtn.classList.add('btn-active')
+    booksPara.classList.toggle('inactive')
+
+    homePara.classList.add('inactive')
+    homeBtn.classList.remove('btn-active')
+}
+
+kindleBtn.addEventListener('click', displaykindle)
+
+function displaykindle(){
+    kindleBtn.classList.add('btn-active')
+    kindlePara.classList.toggle('inactive')
+
+    homePara.classList.add('inactive')
+    homeBtn.classList.remove('btn-active')
+}
